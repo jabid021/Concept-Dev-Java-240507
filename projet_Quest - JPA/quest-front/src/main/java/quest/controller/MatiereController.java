34e8eb8ed35cf2f40b079173ecd3ffdeb10cf3de
Slloth <fiquet.noah@gmail.com>
2024-06-20 17:17:03 +0200

    Matiere

---
+++ projet_Quest - JPA/quest-front/src/main/java/quest/controller/MatiereController.java
@@ -0,0 +1,61 @@
+package quest.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import quest.context.Singleton;
+import quest.model.Matiere;
+
+@WebServlet("/matiere")
+public class MatiereController extends HttpServlet{
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(request.getParameter("id")==null){
+			List<Matiere> matieres = Singleton.getInstance().getDaoMatiere().findAll();
+			request.setAttribute("matieres", matieres);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/matiere/matieres.jsp").forward(request, response);
+		}
+		else if(request.getParameter("delete") != null){
+			int id = Integer.parseInt(request.getParameter("id"));
+			Singleton.getInstance().getDaoMatiere().delete(id);
+
+			response.sendRedirect("matiere");
+		}else{
+			int id = Integer.parseInt(request.getParameter("id"));
+			Matiere matiere = Singleton.getInstance().getDaoMatiere().findById(id);
+			request.setAttribute("matiere", matiere);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/matiere/updateMatiere.jsp").forward(request, response);
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("id") != null){
+			int id = Integer.parseInt(request.getParameter("id"));
+			String libelle = request.getParameter("libelle");
+			int quest = Integer.parseInt(request.getParameter("id"));
+			
+			Matiere matiere = new Matiere(id, libelle, quest);
+	
+			Singleton.getInstance().getDaoMatiere().save(matiere);
+	
+			response.sendRedirect("matiere");
+		}
+		else{
+			String libelle = request.getParameter("libelle");
+			int quest = Integer.parseInt(request.getParameter("id"));
+			
+			Matiere matiere = new Matiere(libelle, quest);
+	
+			Singleton.getInstance().getDaoMatiere().save(matiere);
+	
+			response.sendRedirect("matiere");
+		}
+		
+   }
+}
---
+++ projet_Quest - JPA/quest-front/src/main/webapp/WEB-INF/matiere/updateMatiere.jsp
@@ -0,0 +1,14 @@
+
+<div id="content">
+
+  <h3>Modifier Matiere N°${matiere.id}</h3>
+  <form action="matiere" method="post">
+    <input type="hidden" name="id" value="${matiere.id}">
+    Libelle :<input value="${matiere.libelle}" name="libelle" type="text" placeholder="Saisir le libelle"><br>
+    Code Quest :<input value="${matiere.quest}" name="quest" type="number" placeholder="Saisir le code Quest"><br>
+
+
+    <input class ="btn btn-warning" type="submit" value="Modifier">
+    <a href="matiere"><input type="button" class ="btn btn-info" value="Retour"></a>
+  </form>
+</div>

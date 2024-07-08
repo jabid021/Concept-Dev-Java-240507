0b0860b8837a368add71d7f2aa431f8da35c90db
olivier <o.gozlan@ajc-ingenierie.fr>
2024-07-08 13:04:00 +0200

    demo injection service

--- olivier/SPRING-MVC/exercice1/src/main/java/exercice1/controllers/Exercice.java
+++ olivier/SPRING-MVC/exercice1/src/main/java/exercice1/controllers/Exercice.java
@@ -1,5 +1,6 @@
 package exercice1.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,10 +13,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import exercice1.model.Personne;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import quest.service.StagiaireService;
 
 @Controller
 @RequestMapping("/form")
 public class Exercice {
+	
+	@Autowired
+	private StagiaireService stagiaireSrv;
 
 	@GetMapping("")
 	public String goForm(Model model) {
@@ -35,6 +40,7 @@ public class Exercice {
 
 	@GetMapping("/welcome")
 	public String welcome() {
+		System.out.println(stagiaireSrv);
 		return "exercice1/welcome";
 	}
 }
